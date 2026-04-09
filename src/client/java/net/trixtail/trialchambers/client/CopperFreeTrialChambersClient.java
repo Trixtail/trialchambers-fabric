d9ee9e41d79d41c33c54d961ec33b0e7007da65b
Razor <30379173+Trixtail@users.noreply.github.com>
2026-04-10 00:44:21 +0100

    Add files via upload

---
+++ src/client/java/net/trixtail/trialchambers/client/CopperFreeTrialChambersClient.java
@@ -0,0 +1,10 @@
+package net.trixtail.trialchambers.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperFreeTrialChambersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
